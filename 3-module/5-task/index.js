49ad7016cd6be922a1df8927f18c8a8ec555d885
liannessa <anna.de.sign@yandex.ru>
2023-02-04 22:15:35 +0300

    задачи модуль 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,41 @@
 function getMinMax(str) {
-  // ваш код...
+ const result = {}
+ const array = str.split(' ')
+ const numberArr = array
+  .filter((el) => {
+    return !Number.isNaN(parseFloat(el)) 
+  })
+  .map((el) => {
+    return parseFloat(el)
+  })
+  numberArr.sort((a, b) => {
+    return a - b
+  })
+ result.min = numberArr[0]
+ result.max = numberArr[numberArr.length - 1]
+
+  return result
 }
+
+/* function getMinMax(str) {
+  const array = str.split(' ')
+  const result = array
+   .reduce((acc, curEl) => {
+    const numberCurEl = parseFloat(curEl)
+    if (Number.isNaN(numberCurEl)) {
+      return acc
+    }
+
+    if (numberCurEl < acc.min || !acc.hasOwnProperty('min')) {
+      acc.min = numberCurEl
+    }
+
+    if (numberCurEl > acc.max || !acc.hasOwnProperty('max')) {
+      acc.max = numberCurEl
+    }
+
+    return acc
+   }, {})
+ 
+   return result
+ } */
